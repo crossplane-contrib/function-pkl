41ca7c5ac5532deae4f9cd2d9496b1999de292b9
Tim <32556895+Avarei@users.noreply.github.com>
2024-06-09 19:07:37 +0200

    bump crossplane-example

--- internal/function/fn_test.go
+++ internal/function/fn_test.go
@@ -19,7 +19,7 @@ import (
 )
 
 var (
-	pklPackage = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane-example@0.0.11"
+	pklPackage = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane-example@0.0.12"
 )
 
 func DefaultCRDs() []v1beta1.PklCrdRef {
