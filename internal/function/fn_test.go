7ff94159bfeaf61cc136520cf00ac2e157a2c775
Tim <32556895+Avarei@users.noreply.github.com>
2024-06-12 12:03:10 +0200

    fix expamples

--- internal/function/fn_test.go
+++ internal/function/fn_test.go
@@ -20,7 +20,7 @@ import (
 )
 
 var (
-	pklPackage     = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane-example@0.1.0"
+	pklPackage     = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane-example@0.1.1"
 	pklCorePackage = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane@0.0.18"
 	pklK8sPackage  = "package://pkg.pkl-lang.org/pkl-k8s/k8s@1.0.1"
 )
