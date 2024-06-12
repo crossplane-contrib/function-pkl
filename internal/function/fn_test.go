074d7f721e7cd1617146561a5ecef854d1fbb723
Tim <32556895+Avarei@users.noreply.github.com>
2024-06-12 15:28:49 +0200

    bump version

--- internal/function/fn_test.go
+++ internal/function/fn_test.go
@@ -20,7 +20,7 @@ import (
 
 var (
 	pklPackage     = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane-example@0.1.2"
-	pklCorePackage = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane@0.0.20"
+	pklCorePackage = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane@0.0.21"
 	pklK8sPackage  = "package://pkg.pkl-lang.org/pkl-k8s/k8s@1.0.1"
 )
 
