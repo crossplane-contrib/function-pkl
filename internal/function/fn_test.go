49aa335c40bf98a797e3c819703d23c0bf7a72bf
Tim <32556895+Avarei@users.noreply.github.com>
2024-06-16 05:15:55 +0200

    fix extranomous type annotation

--- internal/function/fn_test.go
+++ internal/function/fn_test.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	pklPackage = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane-example@0.1.13"
+	pklPackage = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane-example@0.1.14"
 )
 
 func TestRunFunction(t *testing.T) {
