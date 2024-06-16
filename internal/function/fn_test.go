c58a1bdf1955cf091cf3e6e1d33d1271039de9a7
Tim <32556895+Avarei@users.noreply.github.com>
2024-06-16 05:11:26 +0200

    bump examples package

--- internal/function/fn_test.go
+++ internal/function/fn_test.go
@@ -18,7 +18,7 @@ import (
 )
 
 var (
-	pklPackage = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane-example@0.1.12"
+	pklPackage = "package://pkg.pkl-lang.org/github.com/avarei/function-pkl/crossplane-example@0.1.13"
 )
 
 func TestRunFunction(t *testing.T) {
