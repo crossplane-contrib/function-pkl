7672e345338f5bbdee8e1c9f130e038543fa2ffe
Tim <32556895+Avarei@users.noreply.github.com>
2024-06-17 23:07:05 +0200

    Add apache license to code files

--- input/generate.go
+++ input/generate.go
@@ -1,12 +1,26 @@
 //go:build generate
 // +build generate
 
+/*
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 // NOTE(negz): See the below link for details on what is happening here.
 // https://github.com/golang/go/wiki/Modules#how-can-i-track-tool-dependencies-for-a-module
 
 // Remove existing and generate new input manifests
 //go:generate rm -rf ../package/input/
-//go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen paths=./v1beta1 object crd:crdVersions=v1 output:artifacts:config=../package/input
+//go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen paths=./v1beta1 object:headerFile=../hack/boilerplate.go.txt crd:crdVersions=v1 output:artifacts:config=../package/input
 
 package input
 
